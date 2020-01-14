d8b10f209e28326250954f4d026c803d982b408b
Melena Suliteanu <melenasuliteanu@gmail.com>
2020-01-14 07:01:34 -0700

    Add functionality to translations cards display english to be translated compare user answer with actual translation on submission convey result through button color then move on

--- src/Lesson/Lesson.jsx
+++ src/Lesson/Lesson.jsx
@@ -1,23 +1,85 @@
 import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import lessons from '../lessons.js'
+import './Lesson.css'
 
 export const Lesson = () => {
   let [translation, setTranslation] = useState("");
-  let text ="Hello world"
+  let [text, setText] = useState("");
+  let [currentCard, setCurrentCard] = useState(0);
+  let [userAnswer, setUserAnswer] = useState("");
+  let [buttonState, setButtonState] = useState({
+    disabled: true,
+    className: "disabled"
+  });
+
+  const { id } = useParams();
+
+
+  const englishText = () => {
+    setText(randomSubarray(lessons[id], 10));
+  }
+
+  const randomSubarray = (arr, size) => {
+    var shuffled = arr.slice(0), i = arr.length, temp, index;
+    while (i--) {
+        index = Math.floor((i + 1) * Math.random());
+        temp = shuffled[index];
+        shuffled[index] = shuffled[i];
+        shuffled[i] = temp;
+    }
+    return shuffled.slice(0, size);
+}
 
   useEffect(() => {
     async function fetchData() {
       
-      let resp = await fetch(`http://localhost:8081/latin/${text}`);
+      let resp = await fetch(`http://localhost:8081/latin/${englishText()}`);
       const translationJson = await resp.json();
-      setTranslation(translationJson[0])
+      setTranslation(translationJson[0]);
     }
     fetchData();
   }, []);
 
+  const handleChange = event => {
+    setUserAnswer(event.target.value);
+    if (event.target.value.length && buttonState.disabled) {
+      setButtonState({buttonState, disabled: false});
+    }
+  }
+
+  const waitAndResetCard = () => {
+    setTimeout(() => {
+      setCurrentCard(currentCard + 1);
+      setUserAnswer("");
+      setButtonState({...buttonState, className: "correct"});
+    }, 1500);
+  }
+
+  const handleSubmit = () => {
+    if (translation === userAnswer) {
+      setButtonState({...buttonState, className: "correct"});
+    } else {
+      setButtonState({...buttonState, className: "incorrect"});
+    }
+    waitAndResetCard();
+  }
+
    return (
-    <article>
-      <div>{text}</div>
-      <div>{translation}</div> 
-    </article>
+     <main>
+       <h4>Translate this sentence</h4>
+      <article className="lesson-card">
+          <div>{text[currentCard]}</div>
+          <textarea placeholder="Type the latin translation"
+                    value={userAnswer}
+                    onChange={handleChange}>
+          </textarea> 
+          <button className={`medium-button ${buttonState.className}`}
+                  onClick={handleSubmit}
+                  disabled={buttonState.disabled}>
+            Check
+          </button>
+      </article>
+     </main>
   )
 }
