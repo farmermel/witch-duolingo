2f1e9e5653801c57fbe60aa84783392098613b80
Melena Suliteanu <melenasuliteanu@gmail.com>
2020-01-14 06:58:27 -0700

    Change format of data to have lesson words in english

---
+++ src/lessons.js
@@ -0,0 +1,78 @@
+export default [
+    [
+        "Hello",
+        "Goodbye",
+        "Good morning",
+        "Good evening",
+        "Goddess be with you",
+        "Nice you meet you",
+        "Excuse me",
+        "My name is Ariadne",
+        "Curse you",
+        "My name is "
+    ],
+    [
+        "Crystal ball",
+        "Cat's skull",
+        "Frog legs",
+        "Tarot cards",
+        "Full moon",
+        "Sacred water",
+        "The old ones",
+        "Scented candle",
+        "Fire",
+        "Earth",
+        "Water"
+    ],
+    [
+        "The parents are sad",
+        "I am happy",
+        "They are very peaceful",
+        "The dog seems sick",
+        "How are you?",
+        "The cat is angry",
+        "Trust",
+        "I feel joy",
+        "What a surprise",
+        "Air"
+    ],
+    [
+        "Hello",
+        "Goodbye",
+        "Good morning",
+        "Good evening",
+        "Goddess",
+        "How are you",
+        "Nice you meet you",
+        "Excuse me",
+        "My name is Ariadne",
+        "Curse you",
+        "My name is "
+    ],[
+        "Hello",
+        "Sun",
+        "Stars",
+        "Good evening",
+        "Wolf",
+        "How are you",
+        "Bat",
+        "Excuse me",
+        "Bird",
+        "Curse you",
+        "My name is "
+    ],
+    [
+        "This is my will",
+        "Let it be so",
+        "Draw the pentagram",
+        "By the power of three",
+        "I conjure you",
+        "Sentences are hard",
+        "Ice",
+        "Rain",
+        "Smoke",
+        "Fog",
+        "Night",
+        "Day"
+    ],
+]
