f1aeb62bd33d58d4fbedf43b63c0d5601445edb7
Melena Suliteanu <melenasuliteanu@gmail.com>
2020-01-15 08:40:54 -0700

    Handle response from server as array rather than string

--- src/Lesson/Lesson.jsx
+++ src/Lesson/Lesson.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import lessons from '../lessons.js'
-import './Lesson.css'
+import '../styles/Lesson.css'
 
 export const Lesson = () => {
-  let [translation, setTranslation] = useState("");
-  let [text, setText] = useState("");
+  let [translation, setTranslation] = useState([]);
+  let [text, setText] = useState([]);
   let [currentCard, setCurrentCard] = useState(0);
   let [userAnswer, setUserAnswer] = useState("");
   let [buttonState, setButtonState] = useState({
@@ -14,11 +14,6 @@ export const Lesson = () => {
   });
 
   const { id } = useParams();
-
-
-  const englishText = () => {
-    setText(randomSubarray(lessons[id], 10));
-  }
 
   const randomSubarray = (arr, size) => {
     var shuffled = arr.slice(0), i = arr.length, temp, index;
@@ -31,12 +26,20 @@ export const Lesson = () => {
     return shuffled.slice(0, size);
 }
 
+  const parseJson = translationJson => {
+    return JSON.parse(translationJson[0].replace(/'/g, '"'))
+  }
+
   useEffect(() => {
+    let theseLessons = randomSubarray(lessons[id], 5);
+    setText(theseLessons);
+    var arrStr = encodeURIComponent(JSON.stringify(theseLessons));
+
     async function fetchData() {
-      
-      let resp = await fetch(`http://localhost:8081/latin/${englishText()}`);
+      let resp = await fetch(`http://localhost:8081/latin/${arrStr}`);
       const translationJson = await resp.json();
-      setTranslation(translationJson[0]);
+      const parsedTranslation = parseJson(translationJson)
+      setTranslation(parsedTranslation);
     }
     fetchData();
   }, []);
@@ -56,8 +59,12 @@ export const Lesson = () => {
     }, 1500);
   }
 
-  const handleSubmit = () => {
-    if (translation === userAnswer) {
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(translation)
+    console.log(translation[currentCard], userAnswer)
+    // TODO deal with random spaces at beginning and end
+    if (translation[currentCard] === userAnswer) {
       setButtonState({...buttonState, className: "correct"});
     } else {
       setButtonState({...buttonState, className: "incorrect"});
@@ -70,15 +77,18 @@ export const Lesson = () => {
        <h4>Translate this sentence</h4>
       <article className="lesson-card">
           <div>{text[currentCard]}</div>
-          <textarea placeholder="Type the latin translation"
-                    value={userAnswer}
-                    onChange={handleChange}>
-          </textarea> 
-          <button className={`medium-button ${buttonState.className}`}
-                  onClick={handleSubmit}
-                  disabled={buttonState.disabled}>
-            Check
-          </button>
+          <form>
+            <textarea placeholder="Type the latin translation"
+                      value={userAnswer}
+                      onChange={handleChange}>
+            </textarea> 
+            <button className={`medium-button ${buttonState.className}`}
+                    onClick={handleSubmit}
+                    disabled={buttonState.disabled}>
+              Check
+            </button>
+          </form>
+          
       </article>
      </main>
   )
